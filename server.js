d2f09137fe11d59946ec08b25f1cd9cdf853af41
Oleksandr <oleksandr.starov@gmail.com>
2016-04-12 07:39:00 +0000

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port =  process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server = http.createServer(function(req, res){
   console.log(req.headers);
     res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><body><h1>Hello World</h1></body></html>');
+  res.end('<html><body><h1>Hello World!!!</h1></body></html>');
   })
 server.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
